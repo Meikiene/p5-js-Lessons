b58c7447cfe39108d5f5be42b54d5673df7f641a
Meikiene <42394095+Meikiene@users.noreply.github.com>
2019-04-04 13:56:55 -1000

    added score system

--- barrier.js
+++ barrier.js
@@ -1,6 +1,6 @@
 function barrier(){
   this.gravity = 1;
-  this.velocity = 8;
+  this.velocity = 12;
   this.height = Math.floor(Math.random() * 257);
   this.width = Math.floor(Math.random() * 257);
   this.x = w - this.width;
@@ -17,13 +17,17 @@ function barrier(){
       this.y = Math.floor((Math.random() * h) + 10);
       this.width = Math.floor(Math.random() * 257);
       this.height = Math.floor(Math.random() * 257);
-
+      score += 1;
+      document.getElementById("scoreID").innerHTML = "Your Score: " + score;
+    }
+    else if (score == 15){
+      this.velocity = 14;
     }
   }
 }
 function barrier2(){
   this.gravity = 1;
-  this.velocity = 8;
+  this.velocity = 10;
   this.height = Math.floor(Math.random() * 257);
   this.width = Math.floor(Math.random() * 257);
   this.x = w - this.width;
@@ -40,6 +44,38 @@ function barrier2(){
       this.y = Math.floor((Math.random() * h) + 10);
       this.width = Math.floor(Math.random() * 257);
       this.height = Math.floor(Math.random() * 257);
+      score += 1;
+      document.getElementById("scoreID").innerHTML = "Your Score: " + score;
+    }
+    else if (score == 15){
+      this.velocity = 12;
+    }
+  }
+}
+function barrier3(){
+  this.gravity = 1;
+  this.velocity = 8;
+  this.height = Math.floor(Math.random() * 257);
+  this.width = Math.floor(Math.random() * 257);
+  this.x = w - this.width;
+  this.y = Math.floor((Math.random() * h) + 10);
+  this.show = function(){
+   fill(color("orange"));
+   rect(this.x,this.y,this.width, this.height);
+  }
+  this.update = function(){
+    //this.velocity += this.gravity; //continually speeds up object to the left
+    this.x -= this.velocity;
+    if (this.x < (0 - this.width)){
+      this.x = w - this.width;
+      this.y = Math.floor((Math.random() * h) + 10);
+      this.width = Math.floor(Math.random() * 257);
+      this.height = Math.floor(Math.random() * 257);
+      score += 1;
+      document.getElementById("scoreID").innerHTML = "Your Score: " + score;
+    }
+    else if (score == 15){
+      this.velocity = 10;
     }
   }
 }
